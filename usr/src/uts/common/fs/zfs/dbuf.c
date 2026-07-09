83185823568eaf50bc77cd7dbf90e20cbb25a232
Lexi Wilson <lexi@cooperi.net>
2026-07-09 11:58:51 +1000

    13317 mismerged; leaked rwlock counts on bonus' parents ensue

--- usr/src/uts/common/fs/zfs/dbuf.c
+++ usr/src/uts/common/fs/zfs/dbuf.c
@@ -1123,6 +1123,7 @@ dbuf_read_impl(dmu_buf_impl_t *db, zio_t *zio, uint32_t flags,
 		if (err != 0) {
 			DB_DNODE_EXIT(db);
 			mutex_exit(&db->db_mtx);
+			dmu_buf_unlock_parent(db, dblt, tag);
 			return (err);
 		}
 
